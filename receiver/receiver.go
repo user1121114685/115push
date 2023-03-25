ddd7e2c10737b145cad279cd01d9782c54129dff
联盟少侠 <admin@shaoxia.xyz>
2023-03-25 23:57:57 +0800

    修复断点续传底层逻辑

--- receiver/receiver.go
+++ receiver/receiver.go
@@ -38,16 +38,13 @@ func Import(dirid, url, shareCid string) {
 		break
 	}
 	wg := NewImporter(5)
-	var step int64 = 0
-	importFileForDir(firstDirID, url, &tickets, wg, &step)
+	importFileForDir(firstDirID, url, &tickets, wg)
 	wg.producerWaitGroupPool.Wait()
 	close(wg.taskChannel)
 
 }
 
-var tmpStep int64 = 0
-
-func importFileForDir(dirid, url string, tickets *utils.FileList, wg *importer, step *int64) {
+func importFileForDir(dirid, url string, tickets *utils.FileList, wg *importer) {
 	// 延迟执行一个匿名函数
 	defer func() {
 		// 调用 recover 来捕获 panic 的值
@@ -56,9 +53,7 @@ func importFileForDir(dirid, url string, tickets *utils.FileList, wg *importer, 
 			log.Println("发生致命错误！！！ 请将错误反馈给开发者，即将开始重新导入....")
 			log.Println(err)
 			time.Sleep(5 * time.Second)
-			tmpStep = *step
-			*step = ^*step + 1 //现在步数是负的了
-			importFileForDir(dirid, url, tickets, wg, step)
+			importFileForDir(dirid, url, tickets, wg)
 		}
 	}()
 	wg.producerWaitGroupPool.Add()
@@ -68,6 +63,11 @@ func importFileForDir(dirid, url string, tickets *utils.FileList, wg *importer, 
 			var id string
 			var err error
 			for i := 0; i < 20; i++ {
+				// 已经导入的部分 直接赋值
+				if *ticket.MakeDIrCid != "" {
+					id = *ticket.MakeDIrCid
+					break
+				}
 				id, err = login.Agent.DirMake(dirid, ticket.ImportTicket.FileName)
 				if err != nil && err.Error() == "target already exists" {
 					// target already exists
@@ -75,29 +75,29 @@ func importFileForDir(dirid, url string, tickets *utils.FileList, wg *importer, 
 					ticket.ImportTicket.FileName = ticket.ImportTicket.FileName + "_" + strconv.Itoa(i)
 					continue
 				}
+				// 没有新建文件夹的ID 新建一次文件夹并赋值
+				ticket.MakeDIrCid = &id
 				break
 			}
-			// 判断是否为断点续传
-			if *step < 0 {
-				*step++
-				continue
-			}
-			if *step == 0 {
-				*step = tmpStep
-				tmpStep = 0
-			}
+
 			var fileList utils.FileList
 
 			getFileList(url+"/115/file_get?cid="+ticket.CID, &fileList)
 			if &fileList == nil {
 				continue
 			}
-			go importFileForDir(id, url, &fileList, wg, step)
+			go importFileForDir(id, url, &fileList, wg)
 			continue
 		}
+		// 如果已经导入 则跳过该文件
+		if *ticket.IsImport {
+			continue
+		}
+
 		// 有时候发生了致命错误，然而不知道罪魁祸首，加上这个一目了然
 		log.Println("准备导入   " + ticket.ImportTicket.FileName)
 		err := login.Agent.Import(dirid, &ticket.ImportTicket)
+		*ticket.IsImport = true
 		if err != nil {
 			if ie, ok := err.(*elevengo.ErrImportNeedCheck); ok {
 				signValue := getCalculateSignValue(url, ticket.PickCode, ie.SignRange)
@@ -106,8 +106,6 @@ func importFileForDir(dirid, url string, tickets *utils.FileList, wg *importer, 
 					ticket.ImportTicket.SignValue = signValue
 					err = login.Agent.Import(dirid, &ticket.ImportTicket)
 					if err != nil {
-						// 不管成功与否 都标记一步 保持计数器一致
-						*step++
 						// 失败重新导入增加那么一丝丝可能性 能降低失败机率
 						reImportFileForDir(dirid, url, ticket.PickCode, ticket.ImportTicket)
 						continue
@@ -115,7 +113,6 @@ func importFileForDir(dirid, url string, tickets *utils.FileList, wg *importer, 
 				}
 			}
 		}
-		*step++
 		log.Println("导入成功   " + ticket.ImportTicket.FileName)
 
 	}
@@ -125,6 +122,7 @@ func importFileForDir(dirid, url string, tickets *utils.FileList, wg *importer, 
 func reImportFileForDir(dirid, url, pickCode string, ImportTicket elevengo.ImportTicket) {
 
 	for i := 0; i < 5; i++ {
+		time.Sleep(1 * time.Second)
 		iport_ticket := ImportTicket
 		err := login.Agent.Import(dirid, &iport_ticket)
 		if err != nil {
--- utils/fileList.go
+++ utils/fileList.go
@@ -9,7 +9,12 @@ type FileList struct {
 
 type SendFile struct {
 	ImportTicket elevengo.ImportTicket `json:"ImportTicket"`
-	CID          string                `json:"CID"`
-	PickCode     string                `json:"pickCode"`
-	IsDir        bool                  `json:"IsDir" default:"false"`
+
+	CID      string `json:"CID"`
+	PickCode string `json:"pickCode"`
+	IsDir    bool   `json:"IsDir" default:"false"`
+	// MakeDIrCid  如果是文件夹，则记录下文件夹的CID ---用于断点续传
+	MakeDIrCid *string `json:"MakeDIrCid"`
+	// IsImport 如果是文件，则记录下文件是否已经导入了。  --- 用于断点续传
+	IsImport *bool `json:"IsImport" default:"false"`
 }
